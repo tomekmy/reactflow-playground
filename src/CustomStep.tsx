03686a6daf096a047634a82ca19c3ced7668d04a
Tomasz Myśliwiec <tomekmy@gmail.com>
2021-04-21 18:12:39 +0200

    Add schema

---
+++ src/CustomStep.tsx
@@ -0,0 +1,44 @@
+// @ts-nocheck
+import React, { memo } from 'react';
+
+import { Handle } from 'react-flow-renderer';
+
+export default memo((params) => {
+  return (
+    <>
+    {
+      params.data.ports?.input.map((handle, idx) => (
+        <Handle
+          key={handle.portId}
+          type="target"
+          position="left"
+          id={handle.portId}
+          style={{ background: '#555', top: `${(100 / (params.data.ports.input.length + 1) * (idx + 1))}%` }}
+          title={handle.desc}
+          onConnect={(params) => console.log('handle onConnect', params)}
+        />
+      ))
+    }
+      <div>
+        <div>{params.data.label}</div>
+        <div style={{fontSize: '8px'}}>
+          { params.data.resId && <>resId: {params.data.resId}</>}
+          { params.data.dictionaryId && <>dictionaryId: {params.data.dictionaryId}</>}
+          { !params.data.resId && !params.data.dictionaryId && <>Dane tymczasowe</> }
+        </div>
+      </div>
+      {
+      params.data.ports?.output.map((handle, idx) => (
+        <Handle
+          key={handle.portId}
+          type="source"
+          position="right"
+          id={handle.portId}
+          style={{ background: '#555', top: `${(100 / (params.data.ports.output.length + 1) * (idx + 1))}%` }}
+          title={handle.desc}
+        />
+      ))
+    }
+    </>
+  );
+});
